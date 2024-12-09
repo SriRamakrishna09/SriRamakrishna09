a5d88ddd4027ab57ee922ae87d1a419e7aa7badb
SriRamakrishna09 <sa2295@srmist.edu.in>
2024-12-09 13:47:16 +0530

    Create Balance checker.py

---
+++ Balance checker.py
@@ -0,0 +1,9 @@
+amtreq, initial = input().split)
+antreq = int(amtreq)
+initial = float(initial)
+if antreq 5 e 0 and amtreg + 0.5 <= initial:
+initial -= (amtreq + 0.5)
+print(f"Current Balance: (initial:.2f")
+else:
+print("Invalid Withdrawal Request")
+print(f"Initial Balance: (initial:.2f)")
